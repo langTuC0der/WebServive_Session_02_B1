a1fcf308f175a95f0225ffa0af29a43566a164d6
Mchinh <chinhmai124@gmail.com>
2026-05-15 14:44:54 +0700

    new commit

---
+++ src/main/java/project/session02_b1/ProductController.java
@@ -0,0 +1,34 @@
+package project.session02_b1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+/*
+    Phân tích lỗi sai của đoạn code:
+    - Trả về String thay vì trả vì list -> Trả về không đúng định dạng như yêu cầu
+    - Chuỗi trả về dạng string -> sai yêu cầu trả về json
+ */
+@RestController
+public class ProductController {
+    static class Product {
+        private String id; private String name; private double price;
+        public Product(String id, String name, double price) { this.id = id; this.name = name; this.price = price; }
+        public String getId() { return id; }
+        public void setId(String id) { this.id = id; }
+        public String getName() { return name; }
+        public void setName(String name) { this.name = name; }
+        public double getPrice() { return price; }
+        public void setPrice(double price) { this.price = price; }
+    }
+
+    @GetMapping("/hot-products")
+    public List<Product> getHotProducts() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product("HP001", "Áo thun 'Code is Life'", 199.000));
+        products.add(new Product("HP002", "Móc khóa 'Bug Free'", 99.000));
+        // Giả sử muốn trả về JSON nhưng lại quên chuyển đổi
+        return products; // <--- Lỗi logic ở đây
+    }
+}
---
+++ src/main/java/project/session02_b1/Session02B1Application.java
@@ -0,0 +1,13 @@
+package project.session02_b1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Session02B1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Session02B1Application.class, args);
+    }
+
+}
